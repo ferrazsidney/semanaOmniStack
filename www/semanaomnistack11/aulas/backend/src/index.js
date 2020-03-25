089b41877095bb6ff8757f8c83f65f27750f7de9
ferrazsidney <ferrazsidney@gmail.com>
2020-03-25 16:47:34 -0300

    Back-end da aplicação

---
+++ www/semanaomnistack11/aulas/backend/src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * rotas/recursos
+ */
+/**
+ * Métodos HTTP
+ * GET: Buscar/Listar informações do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no vack-end
+ * DELETE: Deletar uma informação no back-end
+ */
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rote  após "?" (Filtros Paginação)
+ * Rout Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou auterar os recursos   
+ */
+
+ /**
+  * Banco de Dados:
+  * (Relacionais)SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+  * (Não Relacionais)NoSQL: MongoDB, CouchDB, etc
+  * Vamos usar o SQLite (KNex)
+  */
+
+  /**
+   * Driver de Banco de dados
+   * Driver: SELECT * FROM users
+   * (Usando JavaScript)Query Builder: table('users').select('*').where()
+   * "Usaremos o knex"
+   */
+
+app.listen(3333);
