ba2c09fa6a4150e31dfca22d276c6911a15f91ea
ferrazsidney <ferrazsidney@gmail.com>
2020-03-29 17:15:26 -0300

    Testes de integração e unidade

--- www/semanaomnistack11/aulas/backend/src/app.js
+++ www/semanaomnistack11/aulas/backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -7,7 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
+module.exports = app;
 /**
  * rotas/recursos
  */
@@ -40,4 +43,3 @@ app.use(routes);
    * "Usaremos o knex"
    */
 
-app.listen(3333);
